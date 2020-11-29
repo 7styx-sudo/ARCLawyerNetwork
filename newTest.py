f9ba2949e994858e4f42579976eb14e3093b9a33
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2020-11-29 18:35:08 -0500

    delted migration folder 4 and edited database code

--- newTest.py
+++ newTest.py
@@ -1,12 +1,21 @@
 from ARCLawyerNetworkApp import db,app
 
-from ARCLawyerNetworkApp.Lawyer_Network.models import Lawfirms
+from ARCLawyerNetworkApp.Lawyer_Network.models import Lawfirms, States, serves
 #import numpy as np 
 import pandas as pd 
 
 #my_Lawfirms = Lawfirms('el paso law2', 'texas', 'www.law.com', 'deportation defense', 'law@law.com', 'law.com/contact', 6105554455, 554433343, ' 101 texas drace, texas')
 #db.session.add(my_Lawfirms)
 #db.session.commit()
+
+#this function parses the states columns and makes association between each state and the lawfirm
+def state_association (input,input2):
+	stateslist = input.split(",")#splits states into a list by comma
+	currentfirm = Lawfirms.query.filter_by(lawFirmName= input2)#retriving current lawfirm record from search
+	for eachstate in stateslist:
+		currentstate = States.query.filter_by(states= eachstate)#searches states table for each state in list
+		currentstate.statesServed.append(currentfirm)
+	db.session.commit()
 
 df = pd.read_csv('lawyerDataNew.csv')
 #print(df) 
@@ -26,11 +35,3 @@ for index in length:
 	db.session.commit()
 	state_association(state,lawfirmname)
 
-#this function parses the states columns and makes association between each state and the lawfirm
-def state_association (input,input2):
-	stateslist = input.split(",")#splits states into a list by comma
-	currentfirm = Lawfirms.query.filter_by(lawFirmName= input2)#retriving current lawfirm record from search
-	for eachstate in stateslist:
-		currentstate = States.query.filter_by(states= eachstate)#searches states table for each state in list
-		currentstate.statesServed.append(currentfirm)
-		db.session.commit()
