8e11fac7228f44eb3de4f9206fe6774bcfdb0987
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2021-01-17 01:32:18 -0500

    Update LawyerLogin.py

---
+++ ARCLawyerNetworkApp/Users/LawyerLogin.py
@@ -0,0 +1,15 @@
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import (StringField,SubmitField,PasswordField,validators)
+from wtforms.validators import DataRequired,Email,EqualTo
+
+class guLoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Log In')
+
+	def validate(self):
+
+		
